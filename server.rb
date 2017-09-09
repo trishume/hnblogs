e04729394b2d9fbec2c86b477708fc53f6497d96
Tristan Hume <tris.hume@gmail.com>
2017-09-09 15:03:52 -0400

    Add homepage and custom feeds

--- server.rb
+++ server.rb
@@ -8,7 +8,7 @@ USERS = %w{
   pizlonator pcwalton jblow WalterBright
   chrisseaton DannyBee _yosefk nkurz rsc
   phire tptacek Manishearth Veedrac
-  raphlinus dbaupp gwern patio11
+  raphlinus dbaupp gwern patio11 cperciva
 }
 
 # I don't bother describing people I know of really well
@@ -20,7 +20,12 @@ DESCRIPTIONS = {
   'phire' => 'Dolphin emulator GPU guy',
   'Veedrac' => 'optimization Rust/C++ guy',
   'raphlinus' => 'Raph Levien',
-  'dbaupp' => "Huon Wilson of Rust",
+  'dbaupp' => "Huon Wilson, Rust contributor",
+  'WalterBright' => "Creator of the D language",
+  'pcwalton' => 'Mozilla doing Rust, WebRender, Pathfinder, Servo',
+  'jblow' => 'Jai, The Witness, Braid',
+  'Manishearth' => 'Rust & Servo contributor',
+  'chrisseaton' => 'Author of Truffle Ruby'
 }
 
 class FetchFailed < StandardError
@@ -28,7 +33,11 @@ end
 
 ITEM_TEMPLATE_ERB = <<-HTML
 <%= hit['comment_text'] %>
+<% if hit[:parent] %>
+<h3>Responding to <%= hit[:parent]['by'] %>:</h3>
 
+<%= hit[:parent]['text'] %>
+<% end %>
 <hr>
 <a href="<%= hit['story_url'] %>">Story Link</a>,
 <a href="https://news.ycombinator.com/item?id=<%= hit['story_id'] %>">Story HN Page</a>,
@@ -54,12 +63,37 @@ def item_content(hit)
   ITEM_TEMPLATE.result(binding)
 end
 
-def fetch_data
-  user_tags = USERS.map {|u| "author_#{u}" }.join(',')
-  url = "http://hn.algolia.com/api/v1/search_by_date?tags=comment,(#{user_tags})"
+def fetch_item(id)
+  url = "https://hacker-news.firebaseio.com/v0/item/#{id}.json"
+  response = HTTParty.get(url)
+  item = response.parsed_response
+  raise FetchFailed, response.inspect unless item
+  # pp item
+  item
+end
+
+def fetch_all_parents(hits)
+  threads = []
+
+  hits.each do |hit|
+    if hit['parent_id'] != hit['story_id']
+      threads << Thread.new(hit) do |hit|
+        parent = fetch_item(hit['parent_id'])
+        hit[:parent] = parent
+      end
+    end
+  end
+
+  threads.each(&:join)
+end
+
+def fetch_data(users)
+  user_tags = users.map {|u| "author_#{u}" }.join(',')
+  url = "http://hn.algolia.com/api/v1/search_by_date?hitsPerPage=50&tags=comment,(#{user_tags})"
   response = HTTParty.get(url)
   hits = response['hits']
   raise FetchFailed, response.inspect unless hits
+  fetch_all_parents(hits)
   hits.each do |hit|
     content = item_content(hit)
     hit[:encoded_content] = CGI.escapeHTML(content)
@@ -68,15 +102,23 @@ def fetch_data
 end
 
 get '/' do
-  'Hello world!'
+  data = { users: USERS, descriptions: DESCRIPTIONS }
+  erb :index, locals: data
 end
 
 get '/feed.xml' do
-  hits = fetch_data
+  hits = fetch_data(USERS)
 
-  data = {
-    items: hits
-  }
+  data = {items: hits, page: 'feed.xml'}
+  content_type :atom
+  erb :feed, locals: data
+end
+
+get '/customfeed.xml' do
+  users = params['users'].split(',')
+  hits = fetch_data(users)
+
+  data = {items: hits, page: "customfeed.xml?users=#{params['users']}"}
   content_type :atom
   erb :feed, locals: data
 end
