32b3974d1e9c6e0d3bf6976355cd405dc7598e79
Tristan Hume <tris.hume@gmail.com>
2017-09-09 15:56:54 -0400

    Add gemset.nix

---
+++ gemset.nix
@@ -0,0 +1,66 @@
+{
+  httparty = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0akybx7jkzhhf7f9na12jssfcllma905c8nl93ia89akbbicvq7v";
+      type = "gem";
+    };
+    version = "0.15.6";
+  };
+  multi_xml = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0lmd4f401mvravi1i1yq7b2qjjli0yq7dfc4p1nj5nwajp7r6hyj";
+      type = "gem";
+    };
+    version = "0.6.0";
+  };
+  mustermann = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0wndhnvddjbmvxa7g9a6z3kqcvs7kkjjhgaxwmfxxr57z9wwcsjv";
+      type = "gem";
+    };
+    version = "1.0.1";
+  };
+  puma = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0lkr5zmavyygh2f37avnzllwsgpnmkjfh94cg4sbriyw22kd94qf";
+      type = "gem";
+    };
+    version = "3.10.0";
+  };
+  rack = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "1kczgp2zwcrvp257dl8j4y3mnyqflxr7rn4vl9w1rwblznx9n74c";
+      type = "gem";
+    };
+    version = "2.0.3";
+  };
+  rack-protection = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "04dad1ij5lgjp2pk8ykpxxx2081krmlbh8gii5ip7zpwnc6g2x2m";
+      type = "gem";
+    };
+    version = "2.0.0";
+  };
+  sinatra = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0dbpy8cm06psnk4q1d8map4z81xvlghyi91075xsm56ka2d6kpsi";
+      type = "gem";
+    };
+    version = "2.0.0";
+  };
+  tilt = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0020mrgdf11q23hm1ddd6fv691l51vi10af00f137ilcdb2ycfra";
+      type = "gem";
+    };
+    version = "2.0.8";
+  };
+}
